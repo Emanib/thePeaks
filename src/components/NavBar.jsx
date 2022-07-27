d34143c7ecbb504ffb12ed9b57fc0f3278b77678
eman ebrahim <adla48770@gmail.com>
2022-07-27 23:18:54 +0300

    fix console

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 export default function NavBar({ search, handleChange })
 {
-  console.log(search)
   return (
     <div className ="navbar">
       <div className="logo">
